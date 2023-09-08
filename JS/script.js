82470147f9cb10fac80f6f8bafc89247ba351b9c
pietrolupi <pietro.lupi92@gmail.com>
2023-09-08 14:14:52 +0200

    [edit]: js, html

---
+++ JS/script.js
@@ -0,0 +1,14 @@
+let name = document.getElementById('nome');
+let km = document.getElementById('km');
+const btnGenera = document.getElementById('btn-genera');
+let age= document.getElementById('age');
+let ticketPrice
+
+
+btnGenera.addEventListener('click', function(){
+  
+  console.log(name.value);
+  console.log(km.value);
+  console.log(age.value);
+})
+
