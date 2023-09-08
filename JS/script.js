5f5c2a2f38e183b05f5319980d75111c682addc0
pietrolupi <pietro.lupi92@gmail.com>
2023-09-08 15:17:25 +0200

    [edit]: js, html

--- JS/script.js
+++ JS/script.js
@@ -1,14 +1,38 @@
-let name = document.getElementById('nome');
-let km = document.getElementById('km');
+const inputName = document.getElementById('input-name');
+const inputKm = document.getElementById('input-km');
 const btnGenera = document.getElementById('btn-genera');
 let age= document.getElementById('age');
-let ticketPrice
+let customPrice = inputKm.value * 0.21; 
 
+const ticketSection = document.querySelector('#ticket-section');
 
 btnGenera.addEventListener('click', function(){
   
-  console.log(name.value);
-  console.log(km.value);
-  console.log(age.value);
+  ticketSection.classList.remove('d-none');
+
+  if (isNaN(inputKm.value)){
+    customPrice = '! ERRORE ! Si prega di inserire età e km in formato numerico.'
+  }else if(age.value == 2){
+    customPrice = (customPrice - (customPrice * 0.2)).toFixed(2) + '€';
+  }else if(age.value == 3){
+    customPrice = (customPrice - (customPrice * 0.4)).toFixed(2) + '€';
+  }else{
+    customPrice = customPrice.toFixed(2) + '€';
+  };
+
+
+
+
+  console.log(customPrice);
 })
 
+/* 
+if (isNaN(numKm) || isNaN(age)){
+  customPrice = '! ERRORE ! Si prega di inserire età e km in formato numerico.'
+}else if(age < 18){
+  customPrice = (customPrice - (customPrice * 0.2)).toFixed(2) + '€';
+}else if(age > 65){
+  customPrice = (customPrice - (customPrice * 0.4)).toFixed(2) + '€';
+}else{
+  customPrice = customPrice.toFixed(2) + '€';
+}; */
