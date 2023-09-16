db381ccf4cc58400948d90c7ca955dcc6bb02efd
pietrolupi <pietro.lupi92@gmail.com>
2023-09-16 13:09:02 +0200

    [edit]:js, added functions

--- JS/script.js
+++ JS/script.js
@@ -9,33 +9,58 @@ const ticketSection = document.querySelector('#ticket-section');
 
 btnGenera.addEventListener('click', function(){
   
-  ticketSection.classList.remove('d-none');
+  show(ticketSection);
   let customPrice = inputKm.value * 0.21; 
 
   if (isNaN(inputKm.value)){ 
-    document.querySelector('table').classList.add('d-none');
-    document.querySelector('.error-message').classList.remove('d-none');
+
+    hide(document.querySelector('table'));
+    show(document.querySelector('.error-message'));
+
   }else if(inputKm.value == ''){
+
     customPrice = 'Inserire km da percorrere!'
+
   }else if(age.value == 2){
-    customPrice = (customPrice - (customPrice * 0.2)).toFixed(2) + '€';
-    document.querySelector('.offerta').innerHTML = 'Offerta YOUNG';
+    
+    customPrice = discounter(customPrice, 20);
+    
+    printer(document.querySelector('.offerta'), 'Offerta YOUNG');
+
   }else if(age.value == 3){
-    customPrice = (customPrice - (customPrice * 0.4)).toFixed(2) + '€';
-    document.querySelector('.offerta').innerHTML = 'Offerta SENIOR';
+    
+    customPrice = discounter(customPrice, 40);
+    printer(document.querySelector('.offerta'), 'Offerta SENIOR')
+    
   }else{
     customPrice = customPrice.toFixed(2) + '€';
-    document.querySelector('.offerta').innerHTML = 'Offerta STANDARD';
+    printer(document.querySelector('.offerta'), 'Offerta STANDARD')
   };
 
-
-  document.querySelector('.card-body>h2>span').innerHTML = inputName.value;
-  document.querySelector('.costo').innerHTML = customPrice;
-  document.querySelector('.codice').innerHTML = cpCode;
-  document.querySelector('.carrozza').innerHTML = carriageNum;
+  printer(document.querySelector('.card-body>h2>span'), inputName.value)
+  printer(document.querySelector('.costo'), customPrice)
+  printer(document.querySelector('.codice'), cpCode)
+  printer(document.querySelector('.carrozza'), carriageNum)
 })
 
 btnReset.addEventListener('click', function(){
   ticketSection.classList.add('d-none');
 });
 
+//FUNCTIONS
+
+function printer(where, string){
+  return where.innerHTML = string;
+}
+
+function show(what){
+  return what.classList.remove('d-none');
+}
+
+function hide(what){
+  return what.classList.add('d-none');
+}
+
+function discounter(toDiscount, discountPercentage){
+  return (toDiscount - (toDiscount * (discountPercentage / 100))).toFixed(2) + '€';
+}
